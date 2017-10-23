f50f1f54b4e4abdb7ddcebfe3d37eab027baeaa3
Oleksii Dzhulai <nixlike@gmail.com>
2017-10-23 19:58:27 +0300

    Chamber is able to read secrets on the node

--- cmd/30_openshift.tf
+++ cmd/30_openshift.tf
@@ -1,14 +1,13 @@
 module "master" {
-  source          = "../modules/service"
-  subnet_ids      = ["${module.vpc.external_subnets}"]
-  environment     = "${var.environment}"
-  name            = "master"
-  internal        = false
-  vpc_id          = "${module.vpc.id}"
-  instance_type   = "m4.large"
+  source        = "../modules/service"
+  subnet_ids    = ["${module.vpc.external_subnets}"]
+  environment   = "${var.environment}"
+  name          = "master"
+  internal      = false
+  vpc_id        = "${module.vpc.id}"
+  instance_type = "m4.large"
 
   #CoreOS ami for testing purpose
   ami               = "ami-0d063c6b"
   instance_key_name = "${aws_key_pair.key.key_name}"
 }
-
--- modules/service/30_iam.tf
+++ modules/service/30_iam.tf
@@ -1,6 +1,17 @@
+resource "aws_kms_key" "parameter_store" {
+  description             = "Parameter store kms master key"
+  deletion_window_in_days = 10
+  enable_key_rotation     = true
+}
+
 resource "aws_iam_instance_profile" "provisioner" {
   name  = "provisioner"
   role = "${aws_iam_role.provisioner.name}"
+}
+
+resource "aws_kms_alias" "parameter_store_alias" {
+  name          = "alias/parameter_store_key"
+  target_key_id = "${aws_kms_key.parameter_store.id}"
 }
 
 resource "aws_iam_role" "provisioner" {
@@ -43,6 +54,28 @@ resource "aws_iam_role_policy" "provisioner" {
       ],
       "Effect": "Allow",
       "Resource": "*"
+    },
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Action": [
+        "ssm:GetParameters",
+        "ssm:DescribeParameters"
+      ],
+      "Resource": [
+        "*"
+      ]
+    },
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Action": [
+        "kms:ListKeys",
+        "kms:ListAliases",
+        "kms:Describe*",
+        "kms:Decrypt"
+      ],
+      "Resource": "arn:aws:kms:*:*:key/${aws_kms_key.parameter_store.id}"
     }
   ]
 }
